2e9c27f900821d3e54f3601a4a55c6143a21a07f
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2023-06-30 02:55:37 -0600

    Modify index, script and style
    Changed function names because of conflicts. Added query selectors
    for various elements such as display and buttons. Added functions
    for changing display. Changed display css selector property and
    values.

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function add(a, b) {
+function addNumbers(a, b) {
     return a + b;
 }
 
-function subtract(a, b) {
+function subtractNumbers(a, b) {
     return a - b;
 }
 
-function multiply(a, b) {
+function multiplyNumbers(a, b) {
     return a * b;
 }
 
-function divide(a, b) {
+function divideNumbers(a, b) {
     return a / b;
 }
 
@@ -22,13 +22,91 @@ function operate(operator, firstNumber, secondNumber) {
 
     switch (operator) {
         case 'add':
-            return add(firstNumber, secondNumber)
+            return addNumbers(firstNumber, secondNumber)
         case 'subtract':
-            return subtract(firstNumber, secondNumber)
+            return subtractNumbers(firstNumber, secondNumber)
         case 'multiply':
-            return multiply(firstNumber, secondNumber)
+            return multiplyNumbers(firstNumber, secondNumber)
         case 'divide':
-            return divide(firstNumber, secondNumber)
+            return divideNumbers(firstNumber, secondNumber)
     }
 }
+
+const display = document.querySelector('.display');
+
+let displayValue = 0;
+
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const zero = document.querySelector('.zero');
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const multiply = document.querySelector('.multiply');
+const divide = document.querySelector('.divide');
+const decimal = document.querySelector('.decimal');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
+
+one.addEventListener('click', () => {
+    display.append(1);
+    displayValue += '1';
+});
+
+two.addEventListener('click', () => {
+    display.append(2);
+    displayValue += '2';
+});
+
+three.addEventListener('click', () => {
+    display.append(3);
+    displayValue += '3';
+});
+
+four.addEventListener('click', () => {
+    display.append(4);
+    displayValue += '4';
+});
+
+five.addEventListener('click', () => {
+    display.append(5);
+    displayValue += '5';
+});
+
+six.addEventListener('click', () => {
+    display.append(6);
+    displayValue += '6';
+});
+
+seven.addEventListener('click', () => {
+    display.append(7);
+    displayValue += '7';
+});
+
+eight.addEventListener('click', () => {
+    display.append(8);
+    displayValue += '8';
+});
+
+nine.addEventListener('click', () => {
+    display.append(9);
+    displayValue += '9';
+});
+
+zero.addEventListener('click', () => {
+    display.append(0);
+    displayValue += '0';
+});
+
+
+
+
+
+
 
