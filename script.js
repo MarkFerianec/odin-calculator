d3a4e5ebb5523bfa5c898e07a1d668c241e0aba0
Mark Ferianec <134004771+MarkFerianec@users.noreply.github.com>
2023-07-02 11:11:41 -0600

    Modify index and script
    In index buttons were changed around to be more intuitive. In script
    added and modified multiple variables. Logic for a few buttons was
    implemented for console calculations.

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function divideNumbers(a, b) {
 let firstNumber;
 let secondNumber;
 let operator;
+let buttonToggled = false;  // may not be necessary
 
 function operate(operator, firstNumber, secondNumber) {
 
@@ -34,7 +35,7 @@ function operate(operator, firstNumber, secondNumber) {
 
 const display = document.querySelector('.display');
 
-let displayValue = 0;
+let displayValue = '';
 
 const one = document.querySelector('.one');
 const two = document.querySelector('.two');
@@ -55,58 +56,229 @@ const equals = document.querySelector('.equals');
 const clear = document.querySelector('.clear');
 
 one.addEventListener('click', () => {
-    display.append(1);
+    if (buttonToggled == true) {
+        displayValue = '';
+        display.textContent = '';
+        buttonToggled = false;
+    }
+
+    if (display.textContent != displayValue) {
+        firstNumber = 0;
+        displayValue = '';
+        display.textContent = '';
+        // buttonToggled = false; //may not be neccessary line of code?
+    } //display.textContent > 0 &&  // was in if statement between parantheses previously.
+    // it may and probably is unnecessary.
+
+    // if (display.textContent > 0
+    //     && display.textContent != 1
+    //     && display.textContent != 11
+    //     && display.textContent != 111) {
+    //     firstNumber = 0;
+    //     displayValue = '';
+    //     display.textContent = '';
+    //     // buttonToggled = false; //may not be neccessary?
+    // } //This almost works too, it is getting closer.
+    //This won't work because if a user calculates a 11 it appends a 1 to it making it 111 :(
+
+    // if (display.textContent > 0) {      
+    //     firstNumber = 0;
+    //     displayValue = '';
+    //     display.textContent = '';
+    //     buttonToggled = false; //may not be neccessary?
+    // }
+    //THIS ALMOST WORKS! It just doesn't allow you to enter more than one 1.
+
+
+
+    // if (buttonToggled == false) { //firstNumber > 0 && 
+    //     firstNumber = 0;
+    //     displayValue = '';
+    //     display.textContent = '';
+    // buttonToggled = false
+    // }
+
+    // else if (buttonToggled == false) {  //these 5 lins of code going down are experimental.
+    //     // firstNumber = 0;
+    //     displayValue = '';
+    //     display.textContent = '';
+    // }
+
+    // display.textContent = '';
+
+    // if (secondNumber == 0) {
+    // firstNumber = 0;
+    // displayValue = '';
+    // display.textContent = '';
+    // buttonToggled = false;
+    // }
+
+    // if (buttonToggled == true) { 
+    //     displayValue = '';
+    //     display.textContent = '';
+    //     buttonToggled = false;
+    // }
+
+    // if (buttonToggled != true) {
+    //     display.append(1);       
+    //     displayValue += '1';
+    //     buttonToggled = false; 
+    // }
+
+    // if (display.textContent = 200) {
+    //     display.textContent = '';
+    //     firstNumber = 0;
+    // }
+
+    // if (displayValue.length > 0) {
+    // firstNumber = Number(displayValue);
+    // firstNumber = 1;
+    // display.textContent = '';
+    // display.append(1);
+    // displayValue += '1';
+    // }
+
+    // if (firstNumber > 0) {
+    //     firstNumber = 0;
+    //     firstNumber = display.textContent;
+    //     display.textContent = '';
+    // }
+
+    // firstNumber = 1;
+    //firstNumber = Number(display.textContent);
+    // display.textContent = '';
+
+    display.append(1);       //these 2 lines are important
     displayValue += '1';
 });
 
 two.addEventListener('click', () => {
+    if (buttonToggled == true) {
+        displayValue = '';
+        display.textContent = '';
+        buttonToggled = false;
+    }
     display.append(2);
     displayValue += '2';
 });
 
 three.addEventListener('click', () => {
+    if (buttonToggled == true) {
+        displayValue = '';
+        display.textContent = '';
+        buttonToggled = false;
+    }
     display.append(3);
     displayValue += '3';
 });
 
 four.addEventListener('click', () => {
+    if (buttonToggled == true) {
+        displayValue = '';
+        display.textContent = '';
+        buttonToggled = false;
+    }
     display.append(4);
     displayValue += '4';
 });
 
 five.addEventListener('click', () => {
+    if (buttonToggled == true) {
+        displayValue = '';
+        display.textContent = '';
+        buttonToggled = false;
+    }
     display.append(5);
     displayValue += '5';
 });
 
 six.addEventListener('click', () => {
+    if (buttonToggled == true) {
+        displayValue = '';
+        display.textContent = '';
+        buttonToggled = false;
+    }
     display.append(6);
     displayValue += '6';
 });
 
 seven.addEventListener('click', () => {
+    if (buttonToggled == true) {
+        displayValue = '';
+        display.textContent = '';
+        buttonToggled = false;
+    }
     display.append(7);
     displayValue += '7';
 });
 
 eight.addEventListener('click', () => {
+    if (buttonToggled == true) {
+        displayValue = '';
+        display.textContent = '';
+        buttonToggled = false;
+    }
     display.append(8);
     displayValue += '8';
 });
 
 nine.addEventListener('click', () => {
+    if (buttonToggled == true) {
+        displayValue = '';
+        display.textContent = '';
+        buttonToggled = false;
+    }
     display.append(9);
     displayValue += '9';
 });
 
 zero.addEventListener('click', () => {
+    if (buttonToggled == true) {
+        displayValue = '';
+        display.textContent = '';
+        buttonToggled = false;
+    }
     display.append(0);
     displayValue += '0';
 });
 
+//Operator functions
 
+plus.addEventListener('click', add);
+//added || firstNumber == 0 to this function if statement.
+function add() {
+    if (typeof (firstNumber) == 'undefined' || firstNumber == 0) {
+        firstNumber = Number(displayValue);
+        console.log("first number is " + firstNumber);
+        operator = 'add';
+        buttonToggled = true;
+        if (typeof (secondNumber) == 'number') {
+            firstNumber = firstNumber + secondNumber;
+        }
+    }
+    else {
+        firstNumber = firstNumber + secondNumber;
+        buttonToggled = true;
+    }
+}
 
+equals.addEventListener('click', compute);
 
+function compute() {
+    secondNumber = Number(displayValue);
+    console.log("second number is " + secondNumber);
+    console.log("total is " + (firstNumber + secondNumber));
+    display.textContent = firstNumber + secondNumber;
+    firstNumber = firstNumber + secondNumber;
+    secondNumber = 0;
+}
 
+clear.addEventListener('click', clearDisplay);
 
+function clearDisplay() {
+    firstNumber = undefined;
+    secondNumber = undefined;
+    displayValue = '';
+    display.textContent = '';
 
+}
